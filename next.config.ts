d45dbec1d2bd2afe78ce488a554fcdaa536e0782
waderichman <waderichman444@gmail.com>
2026-02-16 17:25:37 -0800

    remove icon

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,7 @@
-﻿import type { NextConfig } from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  devIndicators: {
-    appIsrStatus: false,
-    buildActivity: false
-  },
+  devIndicators: false,
   experimental: {
     typedRoutes: true
   }
